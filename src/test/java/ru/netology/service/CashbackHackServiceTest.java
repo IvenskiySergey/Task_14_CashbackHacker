c958819a7a489e0aea7c624934d415a9b515746e
Сергей Ивеснкий <ivenskijsv@gmail.com>
2022-07-23 13:12:04 +0300

    Addet testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void Purchase100Test() {
+        CashbackHackService remain = new CashbackHackService();
+        int actual = remain.remain(100);
+
+        Assert.assertEquals(actual, 900);
+    }
+    @Test
+    public void Purchase900Test() {
+        CashbackHackService remain = new CashbackHackService();
+        int actual = remain.remain(900);
+
+        Assert.assertEquals(actual, 100);
+    }
+    @Test
+    public void Purchase1100Test() {
+        CashbackHackService remain = new CashbackHackService();
+        int actual = remain.remain(1100);
+
+        Assert.assertEquals(actual, 900);
+    }
+    @Test
+    public void Purchase1900Test() {
+        CashbackHackService remain = new CashbackHackService();
+        int actual = remain.remain(1900);
+
+        Assert.assertEquals(actual, 100);
+    }
+    @Test
+    public void Purchase1000Test() {
+        CashbackHackService remain = new CashbackHackService();
+        int actual = remain.remain(1000);
+
+        Assert.assertEquals(actual, 0);
+    }
+    @Test
+    public void Purchase2000Test() {
+        CashbackHackService remain = new CashbackHackService();
+        int actual = remain.remain(2000);
+
+        Assert.assertEquals(actual, 0);
+    }
+}
